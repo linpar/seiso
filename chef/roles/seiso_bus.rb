68e327547ac43da9322c92ac99b4101ecba65034
Willie Wheeler <wwheeler@expedia.com>
2014-12-10 00:10:40 -0800

    Added RabbitMQ to Vagrant build

--- chef/roles/seiso_bus.rb
+++ chef/roles/seiso_bus.rb
@@ -1,6 +1,7 @@
 name "seiso_bus"
 description "Seiso message bus"
-run_list "recipe[erlang]"
+run_list "recipe[rabbitmq]", "recipe[rabbitmq::user_management]", "recipe[rabbitmq::mgmt_console]"
+
 override_attributes(
   "erlang" => {
     "install_method" => "package"
