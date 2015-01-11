linpar/seiso

commit a4ed8549eca298ab2c3377005848be9fcc491bd1

Initial work on the v2 API, per issue #23. Also improved formatting of search engine results page per issue #2.

src/main/java/com/expedia/seiso/Seiso.java
@@ -15,121 +15,38 @@
  */
 package com.expedia.seiso;
 
-import java.util.ArrayList;
-
-import lombok.val;
-
-import org.springframework.beans.factory.BeanFactoryUtils;
-import org.springframework.beans.factory.ListableBeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.HttpMessageConvertersAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.data.mapping.context.MappingContext;
-import org.springframework.data.mapping.context.PersistentEntities;
-import org.springframework.data.repository.support.Repositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.Import;
 
-import com.expedia.seiso.core.config.DataSourceProperties;
-import com.expedia.seiso.core.util.ApplicationContextProvider;
-import com.expedia.seiso.domain.repo.RepoPackageMarker;
-import com.zaxxer.hikari.HikariDataSource;
+import com.expedia.seiso.core.config.CustomProperties;
 
+/**
+ * @author Willie Wheeler
+ */
 @Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@EnableAspectJAutoProxy
-@EnableConfigurationProperties
-// @EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
-@EnableJpaRepositories(basePackageClasses = { RepoPackageMarker.class })
-@EnableScheduling
-@EnableTransactionManagement
-public class Seiso {
-	public static final String VERSION = "v1";
+@Import({
+	SeisoDomainConfig.class,
+	SeisoWebConfigBeans.class,
+	SeisoWebConfig.class,
+	SeisoWebSecurityConfig.class
+})
 
-	@Autowired
-	private ListableBeanFactory beanFactory;
+// Exclude HTTP message converters since they expect a single ObjectMapper, while we have two.
+@EnableAutoConfiguration(exclude = { HttpMessageConvertersAutoConfiguration.class })
+
+//@EnableAspectJAutoProxy
+@EnableConfigurationProperties
+public class Seiso {
 	
-	@Autowired
-	private DataSourceProperties dataSourceSettings;
-		
-	@Autowired
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Seiso.class, args);
 	}
-
+	
 	@Bean
-	public SeisoRabbitConfig seisoIntegrationConfig() {
-		return new SeisoRabbitConfig();
-	}
-
-	@Bean
-	public SeisoWebConfig seisoWebConfig() {
-		return new SeisoWebConfig();
-	}
-
-	@Bean
-	public SeisoWebSecurityConfig seisoWebSecurityConfig() {
-		return new SeisoWebSecurityConfig();
-	}
-
-	@Bean
-	public ApplicationContextProvider applicationContextProvider() {
-		return new ApplicationContextProvider();
-	}
-
-	// FIXME DriverManagerDataSource isn't a proper JDBC connection pool. Indeed it is not a pool at all. OK for
-	// development but not for production. Use HikariCP, Tomcat-JDBC, C3P0, etc. instead. Also Apache Commons DBCP
-	// appears to be out of favor nowadays. [WLW]
-	// http://stackoverflow.com/questions/24655247/any-reason-why-spring-hibernate-is-taking-more-time
-	// https://github.com/spring-projects/spring-boot/issues/418
-	@Bean
-	public HikariDataSource dataSource() {
-
-		// FIXME Remove hardcodes
-		// val hikariConfig = new HikariConfig();
-		// hikariConfig.setMaximumPoolSize(10);
-		// hikariConfig.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
-		// hikariConfig.addDataSourceProperty("serverName", "localhost");
-		// hikariConfig.addDataSourceProperty("port", 3306);
-		// hikariConfig.addDataSourceProperty("databaseName", "seiso");
-		// hikariConfig.addDataSourceProperty("user", env.getProperty(SEISO_DB_USERNAME_KEY));
-		// hikariConfig.addDataSourceProperty("password", env.getProperty(SEISO_DB_PASSWORD_KEY));
-		// return new HikariDataSource(hikariConfig);
-
-		// FIXME Legacy configuration.
-		// See https://github.com/brettwooldridge/HikariCP to upgrade.
-		val dataSource = new HikariDataSource();
-		dataSource.setDriverClassName(dataSourceSettings.getDriverClassName());
-		dataSource.setJdbcUrl(dataSourceSettings.getUrl());
-		dataSource.setUsername(dataSourceSettings.getUsername());
-		dataSource.setPassword(dataSourceSettings.getPassword());
-
-		// TODO Add other Hikari data source options.
-
-		return dataSource;
-	}
-
-	@Bean
-	public Repositories repositories() {
-		return new Repositories(beanFactory);
-	}
-
-	@Bean
-	@SuppressWarnings("rawtypes")
-	public PersistentEntities persistentEntities() {
-		val contexts = new ArrayList<MappingContext<?, ?>>();
-		val beans = BeanFactoryUtils.beansOfTypeIncludingAncestors(beanFactory, MappingContext.class);
-		for (val context : beans.values()) {
-			contexts.add(context);
-		}
-		return new PersistentEntities(contexts);
-	}
+	public CustomProperties customProperties() { return new CustomProperties(); }
 }

src/main/java/com/expedia/seiso/domain/entity/listener/NodeIpAddressListener.java
@@ -33,7 +33,7 @@ import com.expedia.seiso.domain.entity.User;
 import com.expedia.seiso.domain.repo.EndpointRepo;
 
 /**
- * @author Willie Wheeler (wwheeler@expedia.com)
+ * @author Willie Wheeler
  */
 @XSlf4j
 public class NodeIpAddressListener {

src/main/java/com/expedia/seiso/domain/entity/listener/ServiceInstancePortListener.java
@@ -30,7 +30,7 @@ import com.expedia.seiso.domain.entity.ServiceInstancePort;
 import com.expedia.seiso.domain.repo.EndpointRepo;
 
 /**
- * @author Willie Wheeler (wwheeler@expedia.com)
+ * @author Willie Wheeler
  */
 @XSlf4j
 public class ServiceInstancePortListener {

commit b312692e9a84f337701c457d8aa907536080949f

Fixed NullPointerException when importing service instance ports.
Commit a4ed8549eca298ab2c3377005848be9fcc491bd1 introduced a regression in
which the app config no longer defined an ApplicationContextProvider bean.
The ServiceInstancePortListener JPA entity listener requires this bean,
and hence was throwing a NullPointerException.

I don't have a good unit test to add here, since we're getting the ACP
dependency via a static method call instead of using dependency injection.
(This has to do with the fact that JPA 2 entity listeners aren't managed
components.) Possible solutions:

* Use AOP to inject required dependencies into unmanaged components.
* Upgrade to JPA 2.1, where entity listeners are managed.

Reported-by: Willie Wheeler
Signed-off-by: Willie Wheeler

src/main/java/com/expedia/seiso/Seiso.java
@@ -26,6 +26,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.expedia.seiso.core.config.CustomProperties;
+import com.expedia.seiso.core.util.ApplicationContextProvider;
 import com.expedia.seiso.web.jackson.hal.HalMapper;
 import com.expedia.seiso.web.jackson.hal.HalModule;
 import com.expedia.seiso.web.jackson.hal.HalResourceAssembler;
@@ -57,6 +58,11 @@ public class Seiso {
 	
 	@Bean
 	public CustomProperties customProperties() { return new CustomProperties(); }
+	
+	@Bean
+	public ApplicationContextProvider applicationContextProvider() {
+		return new ApplicationContextProvider();
+	}
 	
 	// Putting this here since both AMQP and REST API need it.
 	@Bean

src/main/java/com/expedia/seiso/domain/entity/listener/NodeIpAddressListener.java
@@ -33,6 +33,8 @@ import com.expedia.seiso.domain.entity.User;
 import com.expedia.seiso.domain.repo.EndpointRepo;
 
 /**
+ * JPA 2 entity listener to auto-create endpoints after saving a node IP address.
+ * 
  * @author Willie Wheeler
  */
 @XSlf4j
@@ -48,6 +50,8 @@ public class NodeIpAddressListener {
 		if (appContext == null) {
 			this.appContext = ApplicationContextProvider.getApplicationContext();
 		}
+		
+		assert (appContext != null);
 
 		val endpointRepo = appContext.getBean(EndpointRepo.class);
 		val creator = new EndpointCreator(endpointRepo);

src/main/java/com/expedia/seiso/domain/entity/listener/ServiceInstancePortListener.java
@@ -30,6 +30,8 @@ import com.expedia.seiso.domain.entity.ServiceInstancePort;
 import com.expedia.seiso.domain.repo.EndpointRepo;
 
 /**
+ * JPA 2 entity listener to auto-create endpoints after saving a service instance port.
+ * 
  * @author Willie Wheeler
  */
 @XSlf4j
@@ -45,6 +47,8 @@ public class ServiceInstancePortListener {
 		if (appContext == null) {
 			this.appContext = ApplicationContextProvider.getApplicationContext();
 		}
+		
+		assert (appContext != null);
 
 		val endpointRepo = appContext.getBean(EndpointRepo.class);
 		val creator = new EndpointCreator(endpointRepo);

src/test/java/com/expedia/seiso/domain/entity/listener/ServiceInstancePortListenerTests.java
@@ -41,18 +41,18 @@ import com.expedia.seiso.domain.entity.ServiceInstancePort;
 import com.expedia.seiso.domain.entity.listener.ServiceInstancePortListener;
 import com.expedia.seiso.domain.repo.EndpointRepo;
 
+/**
+ * @author Willie Wheeler
+ */
 public class ServiceInstancePortListenerTests {
 	private static final int NUM_IP_ADDRESSES = 2;
 
 	// Class under test
-	@InjectMocks
-	private ServiceInstancePortListener listener;
+	@InjectMocks private ServiceInstancePortListener listener;
 
 	// Dependencies
-	@Mock
-	private ApplicationContext appContext;
-	@Mock
-	private EndpointRepo endpointRepo;
+	@Mock private ApplicationContext appContext;
+	@Mock private EndpointRepo endpointRepo;
 
 	// Test data
 	private List<Node> nodes;