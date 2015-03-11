3fb4da07582695cb85b1ca5a906d9a36c89ae5e8
Willie Wheeler <willie.wheeler@gmail.com>
2015-03-10 23:04:45 -0700

    Small refactor to use FP idioms and HTTP calls via async.js.
    Currently Seiso's JavaScript is written in an imperative style, owing
    largely my inexperience with functional programming idioms. This commit
    includes two changes of note.
    
    First, in homeController, I replaced the for loops with forEach() calls,
    which is more in the functional paradigm. Not sure that what I did is
    idiomatic yet, so happy to take suggestions, but the thought is that we
    migrate more of the code in this direction.
    
    Second, again in homeController, I'm now using async.map() to effect a
    fork/join for the calls to the API. Previousy I was making the calls in
    parallel, but wasn't joining, and so occasionally the service groups
    would be empty when the call to /v1/services completed first.

--- src/main/resources/static/angular/ng-controllers.js
+++ src/main/resources/static/angular/ng-controllers.js
@@ -11,32 +11,49 @@ var pageTitle = function(baseTitle) {
 var homeController = function() {
 	var controller = function($scope, $http) {
 		$scope.model.page.title = pageTitle('Home');
-		serviceGroupsMap = {};
-		$http.get('/v1/service-groups').success(function(data) {
-			$scope.serviceGroups = data;
-			$scope.serviceGroups.push({ "key" : "_ungrouped", "name" : "Ungrouped" });
-			for (i = 0; i < data.length; i++) {
-				serviceGroup = data[i];
-				serviceGroup.services = [];
-				serviceGroupsMap[serviceGroup.key] = serviceGroup;
-			}
-		});
+		$scope.serviceStatus = 'loading';
 		
-		// FIXME If there are more than 300 services, we won't catch them all. We need a JS client for getting the
-		// full list from the paging API. (The API will continue to page.) [WLW]
-		$http.get('/v1/services?page=0&size=300&sort=name').success(function(data) {
-			services = data;
-			for (i = 0; i < services.length; i++) {
-				service = services[i];
-				group = service.group;
-				if (group === null) {
-					serviceGroupsMap['_ungrouped'].services.push(service);
-				} else {
-					serviceGroupsMap[group.key].services.push(service);
-				}
+		var getServiceGroups = function(doneCallback) {
+			$http.get('/v1/service-groups')
+				.success(function(serviceGroups) { return doneCallback(null, serviceGroups); })
+				.error(function(data) { return doneCallback(data, null); });
+		}
+		
+		var getServices = function(doneCallback) {
+			// FIXME If there are more than 300 services, we won't catch them all. We need a JS client for getting the
+			// full list from the paging API. (The API will continue to page.) [WLW]
+			$http.get('/v1/services?page=0&size=300&sort=name')
+				.success(function(services) { return doneCallback(null, services); })
+				.error(function(data) { return doneCallback(data, null); });
+		}
+		
+		var iterator = function(f, doneCallback) { f(doneCallback); }
+		async.map([getServiceGroups, getServices], iterator, function(err, results) {
+			if (err) {
+				$scope.serviceStatus = 'error';
+				return;
 			}
+			
+			var serviceGroups = results[0];
+			var services = results[1];
+			var serviceGroupsMap = {};
+			
+			serviceGroups.push({ "key" : "_ungrouped", "name" : "Ungrouped" });
+			serviceGroups.forEach(function(group) {
+				group.services = [];
+				serviceGroupsMap[group.key] = group;
+			});
+			
+			services.forEach(function(service) {
+				var key = (service.group === null ? '_ungrouped' : service.group.key);
+				serviceGroupsMap[key].services.push(service);
+			});
+			
+			$scope.serviceGroups = serviceGroups;
+			$scope.serviceStatus = 'loaded';
 		});
 	}
+	
 	return [ '$scope', '$http', controller ];
 }
 
@@ -170,7 +187,7 @@ var dataCenterDetailsController = function() {
 				$scope.loadBalancers = dataCenter._embedded.loadBalancers;
 				$scope.model.page.title = pageTitle(dataCenter.name);
 			}
-			var errorHandler = function(data) { alert("Error while getting data center."); }
+			var errorHandler = function() { alert("Error while getting data center."); }
 			v2Api.get('/v2/data-centers/' + $routeParams.key, successHandler, errorHandler);
 		})();
 		
@@ -393,7 +410,7 @@ var serviceInstanceDetailsController = function() {
 				$scope.checks = siEmbedded.seyrenChecks;
 			}
 			// TODO Do better error handling, like the examples below.
-			var errorHandler = function(data) { alert("Error while getting service instance."); }
+			var errorHandler = function() { alert("Error while getting service instance."); }
 			v2Api.get('/v2/service-instances/' + $routeParams.key, successHandler, errorHandler);
 		})();
 		
