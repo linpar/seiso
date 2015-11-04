a532f02c0b2a68b33a6c7cd58a8542fe96d9ba7b
Ian D. McCunn <imccunn@expedia.com>
2015-11-04 10:57:45 -0800

    SI details projection with all properties from enitiy definition

--- src/main/java/com/expedia/seiso/entity/projection/ServiceInstanceDetails.java
+++ src/main/java/com/expedia/seiso/entity/projection/ServiceInstanceDetails.java
@@ -1,15 +1,19 @@
 
 package com.expedia.seiso.entity.projection;
 
+import java.util.List;
+
 import org.springframework.data.rest.core.config.Projection;
 
-import com.expedia.seiso.entity.ServiceInstance;
-import com.expedia.seiso.entity.Environment;
+import com.expedia.seiso.entity.Dashboard;
 import com.expedia.seiso.entity.DataCenter;
+import com.expedia.seiso.entity.Environment;
+import com.expedia.seiso.entity.IpAddressRole;
 import com.expedia.seiso.entity.LoadBalancer;
 import com.expedia.seiso.entity.Node;
 import com.expedia.seiso.entity.Service;
-import java.util.List;
+import com.expedia.seiso.entity.ServiceInstance;
+import com.expedia.seiso.entity.ServiceInstancePort;
 
 /**
  * 
@@ -21,6 +25,8 @@ public interface ServiceInstanceDetails {
 	
 	String getKey();
 	
+	String getDescription();
+	
 	Environment getEnvironment();
 	
 	Service getService();
@@ -29,6 +35,16 @@ public interface ServiceInstanceDetails {
 	
 	LoadBalancer getLoadBalancer();
 	
+	List<IpAddressRole> getIpAddressRoles();
+	
+	List<ServiceInstancePort> getPorts();
+	
 	List<Node> getNodes();
+	
+	Boolean getLoadBalanced();
+	
+	Integer getMinCapacityDeploy();
+	
+	List<Dashboard> getDashboards();
 	
 }
