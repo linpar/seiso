de524a0efc8077b4dd90e77b77278e2796a01577
Wayne Warren <wayne.v.warren@gmail.com>
2016-03-04 08:27:39 -0800

    Added Member entity and supporting sql patch.

---
+++ src/main/sql/patch05.sql
@@ -0,0 +1,14 @@
+-
+-- Table structure for table `membership`
+--
+
+DROP TABLE IF EXISTS `membership`;
+
+CREATE TABLE `membership` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `person_id` smallint(5) unsigned NOT NULL,
+  `group_id` smallint(5) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `membership_person_id` FOREIGN KEY (`person_id`) REFERENCES `person` (`id`),
+  CONSTRAINT `membership_group_id` FOREIGN KEY (`group_id`) REFERENCES `person_group` (`id`)
+);
