d8ea928d207dd9d245db1d90b79231b7ff849eaf
Zaal Tonia <ztonia@launchcg.com>
2016-03-09 14:48:19 -0800

    Use the persistence library instead of annotations

--- src/main/java/com/expedia/seiso/domain/entity/Node.java
+++ src/main/java/com/expedia/seiso/domain/entity/Node.java
@@ -24,6 +24,7 @@ import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -34,8 +35,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Accessors;
-
-import org.springframework.data.annotation.Version;
 
 /**
  * @author Willie Wheeler
