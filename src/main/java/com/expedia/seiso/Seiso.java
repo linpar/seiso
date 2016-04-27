ab693dde13f4726f469f5e90f39f67e53b8f274a
Wayne Warren <wayne.v.warren@gmail.com>
2016-04-27 13:56:31 -0700

    Debugging the endpoint listener

--- src/main/java/com/expedia/seiso/Seiso.java
+++ src/main/java/com/expedia/seiso/Seiso.java
@@ -22,7 +22,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -35,6 +37,8 @@ import com.zaxxer.hikari.HikariDataSource;
  */
 @SpringBootApplication
 @EnableConfigurationProperties
+//@EnableJpaRepositories("com.expedia.seiso.domain.repo")
+//@EntityScan("com.expedia.seiso.domain.entity")   
 public class Seiso {
 	
 	@Autowired
--- src/main/java/com/expedia/seiso/web/eventhandler/RestCallListener.java
+++ src/main/java/com/expedia/seiso/web/eventhandler/RestCallListener.java
@@ -11,22 +11,36 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaContext;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-@Component
+import com.expedia.seiso.domain.entity.Node;
+
+
+@Service
+@Transactional
 public class RestCallListener implements HandlerInterceptor  {
 
-	@PersistenceContext 
+	@Autowired
 	EntityManager entityManager;
+	
+	//@Autowired
+	//SessionFactory sessionFactory;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
+		invokeInterceptor();
+		return true;
+	}
+	
+	private void invokeInterceptor(){
+		//EntityManager entityManager = jpa.getEntityManagerByManagedType(Node.class);
 		Session session = entityManager.unwrap(org.hibernate.Session.class);
 		session.sessionWithOptions().interceptor((new NodeAttributeChangeLogger()));
 		System.out.println("Interceptor registered");
-		return true;
 	}
 	
 	@Override
