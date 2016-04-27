f154405c5f5f59b97106b6a7329e6f4ff189e45a
Wayne Warren <wayne.v.warren@gmail.com>
2016-04-27 09:17:38 -0700

    Inserted a mediator to pass all calls to our Interceptor

--- src/main/java/com/expedia/seiso/web/eventhandler/RestCallListener.java
+++ src/main/java/com/expedia/seiso/web/eventhandler/RestCallListener.java
@@ -1,11 +1,15 @@
 package com.expedia.seiso.web.eventhandler;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,13 +17,14 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class RestCallListener implements HandlerInterceptor  {
 
-	@Autowired
-    private SessionFactory sessionFactory;
+	@PersistenceContext 
+	EntityManager entityManager;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 		throws Exception {
-		Session session = this.sessionFactory.withOptions().interceptor(new NodeAttributeChangeLogger()).openSession();		
+		Session session = entityManager.unwrap(org.hibernate.Session.class);
+		session.sessionWithOptions().interceptor((new NodeAttributeChangeLogger()));
 		System.out.println("Interceptor registered");
 		return true;
 	}
