c1e73d363b69ce1e9ba0eed2cbf2afe81cdca723
Wayne Warren <wayne.v.warren@gmail.com>
2016-05-17 09:09:59 -0700

    Integrated Willie's most recent YAML fix.  Works fine.

---
+++ src/main/java/com/expedia/seiso/hbn/NodeAttributeChangeLogger.java
@@ -0,0 +1,45 @@
+package com.expedia.seiso.hbn;
+
+import java.io.Serializable;
+
+import org.hibernate.EmptyInterceptor;
+import org.hibernate.type.Type;
+
+import com.expedia.seiso.domain.entity.Node;
+
+import lombok.extern.slf4j.XSlf4j;
+
+@XSlf4j
+public class NodeAttributeChangeLogger extends EmptyInterceptor {
+    
+	private static final long serialVersionUID = 10009L;
+
+    @Override
+    public boolean onFlushDirty(Object entity,
+                                Serializable id,
+                                Object[] currentState,
+                                Object[] previousState,
+                                String[] propertyNames,
+                                Type[] types) {
+    	boolean updated = false;
+    	StringBuilder content = new StringBuilder();
+    	if (entity instanceof Node){
+    		for ( int i=0; i < propertyNames.length  && !updated; i++ ) {
+    			if (currentState[i] != null){
+	                if ( !currentState[i].equals(previousState[i]) && !propertyNames[i].equals("healthState")) {
+	                	if (!updated){
+	                		content.append("Node changed:\r\n");
+	                		updated = true;
+	                	} 
+	                	content.append("   Attribute: " + propertyNames[i] + "," + " previous value: " + 
+	                			previousState[i].toString() + ", current value: " + currentState[i]);
+	                }
+    			}
+            }
+        	if (updated){
+        		log.debug(content.toString());
+        	}
+    	}
+        return false;
+    }
+}	
